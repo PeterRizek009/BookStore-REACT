19d156fe482fe7d93e818df555a7b4c8ab0225da
Peter Shahin <petershahin00@gmail.com>
2022-11-28 20:59:06 +0200

    editing

--- src/component/aboutus/aboutus.jsx
+++ src/component/aboutus/aboutus.jsx
@@ -14,11 +14,11 @@ const AboutUs = () => {
           </div>
           </div>
             <div className="about mx-auto">
-              <div className="item">
+              <div className="about-item">
                 <h4 className="mx-3">Book store APP</h4>
                 <p className="mx-auto pt-5">book store app is shopping app built mainly br React JS and other technologies </p>
               </div>
-              <div className="item">
+              <div className="about-item">
                 <h4 className="mx-3">Technologies used</h4>
                 <ul className="mx-4">
                   <li>React JS</li>
@@ -27,7 +27,7 @@ const AboutUs = () => {
                   <li>Bootstrap 5</li>
                 </ul>
               </div>
-              <div className="item">
+              <div className="about-item">
                 <h4 className="mx-3">Follow me on</h4>
                 <section className="mx-auto" >
                   <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="https://www.linkedin.com/in/peter-rezik-a84408123/" role="button" data-mdb-ripple-color="dark"><i className="fab fa-linkedin" /></a>
@@ -35,7 +35,7 @@ const AboutUs = () => {
                 </section>
               </div>
 
-              <div className="item">
+              <div className="about-item">
                 <h4 className="mx-3">My portfolio</h4>
               </div>
             </div>
