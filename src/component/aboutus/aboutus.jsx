2d8d17b08f374898c7ff81ef56af4118d8d34852
Peter Shahin <petershahin00@gmail.com>
2022-11-28 20:44:23 +0200

    about us

--- src/component/aboutus/aboutus.jsx
+++ src/component/aboutus/aboutus.jsx
@@ -4,21 +4,45 @@ import "./aboutus.css";
 
 const AboutUs = () => {
   return (
-  <div>
-    <div className="b-example-divider" />
-    <div className="text-secondary px-4 py-5 aboutus-bg">
-      <div className="py-4">
-        <div className="titleabout fw-bold text-center">
-          <h2>
-            About <b>Book Store  APP</b>
-          </h2>
-        </div>
-     
+    <div>
+      <div className="text-secondary p-2 ">
+        
+          <div className="titleabout fw-bold text-center">
+            <h2>
+              About <b>Book Store  APP</b>
+            </h2>
+          </div>
+          </div>
+            <div className="about mx-auto">
+              <div className="item">
+                <h4 className="mx-3">Book store APP</h4>
+                <p className="mx-auto pt-5">book store app is shopping app built mainly br React JS and other technologies </p>
+              </div>
+              <div className="item">
+                <h4 className="mx-3">Technologies used</h4>
+                <ul className="mx-4">
+                  <li>React JS</li>
+                  <li>Framer Motion Library</li>
+                  <li>CSS</li>
+                  <li>Bootstrap 5</li>
+                </ul>
+              </div>
+              <div className="item">
+                <h4 className="mx-3">Follow me on</h4>
+                <section className="mx-auto" >
+                  <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="https://www.linkedin.com/in/peter-rezik-a84408123/" role="button" data-mdb-ripple-color="dark"><i className="fab fa-linkedin" /></a>
+                  <a className="btn btn-link btn-floating btn-lg text-dark m-1" href="https://github.com/PeterRizek009" role="button" data-mdb-ripple-color="dark"><i className="fab fa-github" /></a>
+                </section>
+              </div>
 
-      </div>
-    </div>
-    
-  </div>
+              <div className="item">
+                <h4 className="mx-3">My portfolio</h4>
+              </div>
+            </div>
+        </div>
+
+
+  
   );
 }
 
