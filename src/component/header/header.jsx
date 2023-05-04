57b4a6e99ddd7453903724ddc196dad81a058f29
Peter Shahin <petershahin00@gmail.com>
2023-05-04 20:36:49 +0300

    edit

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -8,15 +8,13 @@ const Header = () => {
         for (let i = 1; i < 4; i++) {
             items.push(
                 <div className={i === 1 ? 'carousel-item active' : 'carousel-item'} >
-                    <img id={`img${i}`} alt=""/>
+                    <img id={`img${i}`} alt="" />
                     <div className="img-text">
                         <h2>
                             Alex <b> Book Store</b>
                         </h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem
-                            minus minima similique error ad inventore quia perferendis
-                            corrupti, eligendi doloribus.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima maxime omnis sed nostrum, porro harum d.
                         </p>
                     </div>
                 </div>
@@ -24,27 +22,27 @@ const Header = () => {
         }
         return items;
     }
-    return ( 
-         <React.Fragment>
-        <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to={0} className="active" aria-current="true" aria-label="Slide 1" />
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to={1} aria-label="Slide 2" />
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to={2} aria-label="Slide 3" />
-            </div>
-            <div className="carousel-inner">
-                {generateCarouselItem()}
-            </div>
-           
+    return (
+        <React.Fragment>
+            <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to={0} className="active" aria-current="true" aria-label="Slide 1" />
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to={1} aria-label="Slide 2" />
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to={2} aria-label="Slide 3" />
+                </div>
+                <div className="carousel-inner">
+                    {generateCarouselItem()}
+                </div>
 
-        </div>
-    </React.Fragment> );
+
+            </div>
+        </React.Fragment>);
 }
- 
+
 export default Header;
 
 
-  
+
 
 
 
