ffbb83a3212a3a14dff2c5e6811bcd47f4216a41
Peter Shahin <petershahin00@gmail.com>
2023-06-05 23:05:10 +0300

    edit book details

--- src/component/bookdetails/bookdetails.jsx
+++ src/component/bookdetails/bookdetails.jsx
@@ -1,7 +1,7 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import images from './../../images';
-
+import './bookdetails.css'
 
 const BookDetails = ({ onSave , books , onWishlist}) => {
 
@@ -13,26 +13,24 @@ const BookDetails = ({ onSave , books , onWishlist}) => {
     const imageStyle = {
         width: "250px",
         height: "400px",
-        border: "2px solid black",
-        padding: "10px",
+        padding: "2px",
     }
     return (
-        <Fragment>
+        
             <section className="py-5">
-                <div className="container px-4 px-lg-5 my-5">
-                    <div className="row gx-4 gx-lg-5 align-items-center">
+                <div className="container">
+                    <div className="bookdetails">
                         <div className="col-md-4">
-                            <img className="card-img-top mb-5 mb-md-0" style={imageStyle} src={images[id]} alt="" />
+                            <img className="card-img-top mb-5 mb-md-0" style={imageStyle} src={images[id]} alt="img" />
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-md-8 p-2">
                             <h1 className="display-5 fw-bolder">{book.name}</h1>
                             <div className="fs-5 mb-5">
                                 <span className="text">{`Price : ${book.price}$`}</span>
                                 <span className='m-5' style={{ color: "green" }}>Sale : 25%</span>
                             </div>
-                            <p className="lead" style={{ color: "black" }}>Lorem ipsum dolor sit amet
-                                consectetur adipisicing elit. Praesentium at dolorem quidem modi.
-                                Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
+                            <p className="lead mb-5 w-50" style={{ color: "black" }}>Lorem ipsum dolor sit amet
+                                consectetur adipisicing elit. Praesentium at dolorem quidem </p>
                             <div className="d-flex ">
 
                                 <button className="btn btn-danger flex-shrink-1" onClick={() => onSave(book)} type="button">
@@ -48,7 +46,7 @@ const BookDetails = ({ onSave , books , onWishlist}) => {
                     </div>
                 </div>
             </section>
-        </Fragment>
+      
     );
 }
 
